daa666ccb3f3f178d76d3758166c9abe59e617e1
Ajith Looka <ajith.looka@moloco.com>
2026-03-15 16:47:49 -0700

    release 4.4.1

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// Package.swift
+// swift-tools-version: 5.6
+import PackageDescription
+
+let package = Package(
+  name: "MolocoSDK",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(name: "MolocoSDK", targets: ["MolocoSDK"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "MolocoSDK",
+      url: "https://moloco-ios-build.s3.amazonaws.com/moloco-sdk/MolocoSDK-4.4.1.zip",
+      checksum: "78c8b918ba3f52c5953267d70c0588515304c34735a2a44265878cb7007c0578"
+    )
+  ]
+)
