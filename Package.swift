43683aabc01a00feaca63f7ebdb8b5d843694e64
msdk <msdk@github.com>
2026-03-27 20:22:29 +0000

    Release 4.5.1

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "MolocoSDK",
-      url: "https://moloco-ios-build.s3.amazonaws.com/moloco-sdk/MolocoSDK-4.5.0.zip",
-      checksum: "d9fbdc7102461bdc810fe2ac8f3e6aa07780ea1b4aebdc852e9aec16ca607528"
+      url: "https://moloco-ios-build.s3.amazonaws.com/moloco-sdk/MolocoSDK-4.5.1.zip",
+      checksum: "f52fda7bb7ed8199614228187bf2c0f2377e8d0e7f8f472f2e52eb1a4ab66cf5"
     )
   ]
 )
